a19fa393250cbb128b156cea07248b64ac90690a
Kunal Garg <kunal.garg@razorpay.com>
2024-10-10 18:19:27 +0530

    added complete verification api

--- src/App.tsx
+++ src/App.tsx
@@ -26,8 +26,16 @@ function App() {
 
   function base64ToArrayBuffer(base64Input) {
    
-    const bytes = new Uint8Array(base64Input);
-    return bytes
+    // Replace URL-safe characters and add padding
+   const padding = '='.repeat((4 - base64Input.length % 4) % 4);
+   const base64 = (base64Input + padding).replace(/-/g, '+').replace(/_/g, '/');
+   const binaryString = atob(base64);
+   const len = binaryString.length;
+   const bytes = new Uint8Array(len);
+   for (let i = 0; i < len; i++) {
+       bytes[i] = binaryString.charCodeAt(i);
+   }
+   return bytes.buffer; // Return ArrayBuffer
   
   }
 
@@ -128,43 +136,96 @@ function App() {
   };
 
 
+  async function fetchVerifyOptions() {
+    try {
+      const response = await fetch(
+        'http://localhost:8080/create_verify_options',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      const responseData = await response.json();
+      console.log(
+        '🚀 ~ fetchVerifyOptions ~ response:',
+        responseData.publicKey.allowCredentials[0]
+      );
+      return responseData.publicKey;
+    } catch (error) {
+      console.error('Error:', error);
+      throw error;
+    }
+  }
 
-  // const verifyPasskey = async () => {
-  //   const publicKeyCredentialRequestOptions = {
-  //     challenge: new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7]),
-  //     allowCredentials: [
-  //       {
-  //         id: rawId,
-  //         type: "public-key",
-  //         // transports: ['usb', 'ble', 'nfc'],
-  //       },
-  //     ],
-  //     rpId: window.location.hostname,
-  //     timeout: 60000,
-  //   };
+  async function completeVerification(assertion) {
+    try {
+      const response = await fetch('http://localhost:8080/complete_verification', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body:  JSON.stringify(assertion),
+      });
 
-  //   const assertion = await navigator.credentials.get({
-  //     publicKey: publicKeyCredentialRequestOptions,
-  //   });
-  //   console.log("🚀 ~ verifyPasskey from webauthn directly is  ~ assertion:", assertion);
+      if (!response.ok) {
+        throw new Error('Failed to make request');
+      }
 
-  //   handleSave(
-  //     {
-  //       id: assertion.id,
-  //       raw_id: arrayBufferToBase64(assertion.rawId),
-  //       type: assertion.type,
-  //       response: {
-  //         client_data_json: arrayBufferToBase64(assertion.response.clientDataJSON),
-  //         authenticator_data:arrayBufferToBase64(assertion.response.authenticatorData),
-  //         signature: arrayBufferToBase64(assertion.response.signature),
-  //         user_handle: arrayBufferToBase64(assertion.response.userHandle),
-  //       }
-  //     }
-  //   );
+      const data = await response.json();
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
 
-  
 
-  // };
+  const verifyPasskey = async () => {
+    // const publicKeyCredentialRequestOptions = {
+    //   challenge: new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7]),
+    //   allowCredentials: [
+    //     {
+    //       id: rawId,
+    //       type: "public-key",
+    //       // transports: ['usb', 'ble', 'nfc'],
+    //     },
+    //   ],
+    //   rpId: window.location.hostname,
+    //   timeout: 60000,
+    // };
+
+    const verifyOptions = await fetchVerifyOptions();
+    console.log("🚀 ~ verifyPasskey ~ verifyOptions:", verifyOptions)
+
+    verifyOptions.challenge = await base64ToArrayBuffer(
+      verifyOptions.challenge
+    );
+
+    verifyOptions.allowCredentials[0].id = await base64ToArrayBuffer(
+      verifyOptions.allowCredentials[0].id
+    );
+
+    console.log("🚀 ~ afer modification ~ verifyOptions:", verifyOptions)
+
+    const assertion = await navigator.credentials.get({
+      publicKey: verifyOptions,
+    });
+    console.log("🚀 ~ verifyPasskey from webauthn directly is  ~ assertion:", assertion);
+    completeVerification(assertion);
+    // handleSave(
+    //   {
+    //     id: assertion.id,
+    //     raw_id: arrayBufferToBase64(assertion.rawId),
+    //     type: assertion.type,
+    //     response: {
+    //       client_data_json: arrayBufferToBase64(assertion.response.clientDataJSON),
+    //       authenticator_data:arrayBufferToBase64(assertion.response.authenticatorData),
+    //       signature: arrayBufferToBase64(assertion.response.signature),
+    //       user_handle: arrayBufferToBase64(assertion.response.userHandle),
+    //     }
+    //   }
+    // );
+  };
 
 
 
@@ -174,7 +235,7 @@ function App() {
       <div>
         <CardPaymentForm
           createPasskey={registerUser}
-          // verifyPasskey={verifyPasskey}
+          verifyPasskey={verifyPasskey}
         />
       </div>
     </>
